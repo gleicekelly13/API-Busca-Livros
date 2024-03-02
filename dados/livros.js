08e6ca5a0e9bceae223e5f6c49b7e4d26dfdc47f
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-03-02 14:37:11 -0300

    Create livros.js

---
+++ dados/livros.js
@@ -0,0 +1,11 @@
+const livros = [
+    {id: 1, titulo: "O poder do hábito", autor: "Charles Duhigg", ano: 2012},
+    {id: 2, titulo: "Hábitos atômicos", autor: "James Clear", ano: 2019},
+    {id: 3, titulo: "Essencialismo", autor: "Greg Mckeown", ano: 2015},
+    {id: 4, titulo: "A única coisa", autor: "Gary Keller", ano: 2021},
+    {id: 5, titulo: "Sem esforço", autor: "Greg Mckeown", ano: 2021},
+    {id: 6, titulo: "Nada pode me ferir", autor: "David Goggins", ano: 2023},
+    {id: 7, titulo: "O homem mais rico da Babilônia", autor: "George S. Clason", ano: 1926}
+]
+
+exports.livros = livros;
