3031c2f079b19828ee427bb884a83ae75be25971
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-03-04 17:51:54 -0300

    Update servico.js

--- servicos/servico.js
+++ servicos/servico.js
@@ -4,10 +4,16 @@ function buscarLivros() {
     return livros;
 };
 
+function buscarLivrosPorNome(titulo) {
+    let livroBuscado = livros.filter((livro) => livro.titulo.toLowerCase().includes(titulo.toLowerCase()));
+    return livroBuscado;
+};
+
 function buscarLivrosPorId(id) {
-    let livroId = livros.find((livro) => livro.id ===id)
+    let livroId = livros.find((livro) => livro.id === id)
     return livroId;
-}
+};
 
 exports.buscarLivros = buscarLivros;
+exports.buscarLivrosPorNome = buscarLivrosPorNome;
 exports.buscarLivrosPorId = buscarLivrosPorId;
